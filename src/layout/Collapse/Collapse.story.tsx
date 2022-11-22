3f98f9f85e364c4bb6492dd9479a6e7d13e70909
Austin <amcdaniel2@gmail.com>
2022-11-22 12:23:19 -0500

    add collapse

---
+++ src/layout/Collapse/Collapse.story.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { Collapse } from './Collapse';
+
+export default {
+  title: 'Layout/Collapse',
+  component: Collapse
+};
+
+export const Simple = () => {
+  const [expanded, setExpanded] = useState<boolean>(false);
+
+  return (
+    <div style={{ width: 300, height: 300, textAlign: 'center' }}>
+      <button type="button" onClick={() => setExpanded(!expanded)}>
+        Expand Contents
+      </button>
+      <Collapse expanded={expanded}>
+        <h1>Hello</h1>
+        <p>The quick fox jumped over the fence</p>
+        <p>The quick fox jumped over the fence</p>
+        <p>The quick fox jumped over the fence</p>
+      </Collapse>
+    </div>
+  );
+};
---
+++ src/layout/Collapse/Collapse.tsx
@@ -0,0 +1,52 @@
+import React, { FC, PropsWithChildren } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import classNames from 'classnames';
+import isFunction from 'lodash/isFunction';
+import css from './Collapse.module.css';
+
+const VARIANTS = {
+  open: {
+    opacity: 1,
+    height: 'auto'
+  },
+  collapsed: {
+    opacity: 0,
+    height: 0
+  }
+};
+
+const TRANSITION = {
+  duration: 0.5,
+  ease: [0.04, 0.62, 0.23, 0.98],
+  when: 'beforeChildren'
+};
+
+export interface CollapseProps
+  extends Omit<Partial<HTMLDivElement>, 'children'> {
+  expanded?: boolean;
+  children?: React.ReactNode | (() => React.ReactNode);
+}
+
+export const Collapse: FC<CollapseProps> = ({
+  children,
+  expanded,
+  className,
+  ...rest
+}) => (
+  <AnimatePresence initial={false}>
+    {expanded && (
+      <motion.section
+        {...(rest as any)}
+        className={classNames(className, css.section)}
+        key="content"
+        initial="collapsed"
+        animate="open"
+        exit="collapsed"
+        variants={VARIANTS}
+        transition={TRANSITION}
+      >
+        {isFunction(children) ? children() : children}
+      </motion.section>
+    )}
+  </AnimatePresence>
+);
