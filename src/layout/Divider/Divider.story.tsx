8b786dd7696a12aa74dfc53d9a9486b45f6e1369
Austin <amcdaniel2@gmail.com>
2022-12-30 12:44:37 +0800

    improve divider story

--- src/layout/Divider/Divider.story.tsx
+++ src/layout/Divider/Divider.story.tsx
@@ -1,51 +1,41 @@
 import React from 'react';
-import {
-  DesignTokens,
-  DesignTokensProvider,
-  lightTheme
-} from '../../utils/DesignTokens';
+import { DesignTokensProvider, lightTheme } from '../../utils/DesignTokens';
 import { Divider } from './Divider';
 
 export default {
   title: 'Layout/Divider',
-  component: Divider
-};
-
-const dividerTheme: DesignTokens = {
-  ...lightTheme,
-  components: {
-    divider: {
-      'divider-bg': lightTheme.colors.grey['500']
-    }
-  }
+  component: Divider,
+  decorators: [
+    Story => (
+      <DesignTokensProvider value={lightTheme}>
+        <div style={{ width: 400 }}>
+          <Story />
+        </div>
+      </DesignTokensProvider>
+    )
+  ]
 };
 
 export const Horizontal = () => (
-  <DesignTokensProvider value={dividerTheme}>
-    <div style={{ width: 400 }}>
-      Above
-      <Divider />
-      Below
-    </div>
-  </DesignTokensProvider>
+  <>
+    Above
+    <Divider />
+    Below
+  </>
 );
 
 export const NoMargin = () => (
-  <DesignTokensProvider value={dividerTheme}>
-    <div style={{ width: 400 }}>
-      Above
-      <Divider disableMargins />
-      Below
-    </div>
-  </DesignTokensProvider>
+  <>
+    Above
+    <Divider disableMargins />
+    Below
+  </>
 );
 
 export const Vertical = () => (
-  <DesignTokensProvider value={dividerTheme}>
-    <div style={{ height: 400, width: 100, display: 'flex' }}>
-      Left
-      <Divider orientation="vertical" />
-      Right
-    </div>
-  </DesignTokensProvider>
+  <div style={{ height: 400, display: 'flex' }}>
+    Left
+    <Divider orientation="vertical" />
+    Right
+  </div>
 );
