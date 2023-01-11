512d4fe8c6301f8adaaf89845ceb36f07ed53e7d
Stephanie Yang <steph@goodcode.us>
2023-01-10 18:05:41 -0600

    style chip component

--- src/elements/Chip/Chip.story.tsx
+++ src/elements/Chip/Chip.story.tsx
@@ -7,27 +7,37 @@ export default {
 };
 
 export const Colors = () => (
-  <div>
+  <div style={{ display: 'flex', alignItems: 'center', gap: 10 }}>
     <Chip>Default</Chip>
     <Chip color="primary">Primary</Chip>
     <Chip color="secondary">Secondary</Chip>
     <Chip color="error">Error</Chip>
+    <Chip color="success">Warning</Chip>
     <Chip color="warning">Warning</Chip>
     <Chip color="info">Info</Chip>
   </div>
 );
 
-export const Variants = () => (
-  <>
-    <Chip variant="filled">Filled</Chip>
-    <Chip variant="outline">Outline</Chip>
-  </>
-);
-
 export const Sizes = () => (
-  <div>
+  <div style={{ display: 'flex', alignItems: 'center', gap: 10 }}>
     <Chip size="small">Small</Chip>
     <Chip>Medium</Chip>
     <Chip size="large">Large</Chip>
+  </div>
+);
+
+export const Variants = () => (
+  <div style={{ display: 'flex', alignItems: 'center', gap: 10 }}>
+    <Chip variant="filled">Filled</Chip>
+    <Chip variant="outline">Outline</Chip>
+  </div>
+);
+
+export const Selectable = () => (
+  <div style={{ display: 'flex', alignItems: 'center', gap: 10 }}>
+    <Chip onClick={() => console.log('click!')} selected>
+      Selected
+    </Chip>
+    <Chip onClick={() => console.log('click!')}>Unselected</Chip>
   </div>
 );
