3c25d02ba733ff09d8743b648f4511f1e18eec0b
amcdnl <amcdaniel2@gmail.com>
2023-06-14 15:22:48 -0400

    fix exports

--- vite.config.ts
+++ vite.config.ts
@@ -33,10 +33,11 @@ export default defineConfig(({ mode }) =>
       build: {
         minify: false,
         sourcemap: true,
+        copyPublicDir: false,
         lib: {
           entry: resolve('src', 'index.ts'),
           name: 'reablocks',
-          fileName: 'reablocks'
+          fileName: 'index'
         },
         rollupOptions: {
           plugins: [
