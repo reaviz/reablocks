d76c8f15ed00afb6f236223d1a865c0d52e5df98
Stephanie Yang <steph@goodcode.us>
2023-09-26 17:16:44 -0500

    add doc

--- src/form/Input/InlineInput/InlineInput.tsx
+++ src/form/Input/InlineInput/InlineInput.tsx
@@ -4,12 +4,44 @@ import classNames from 'classnames';
 import { InputRef } from '../Input';
 import css from './InlineInput.module.css';
 
-export const InlineInput: FC<InputHTMLAttributes<HTMLInputElement>> =
-  forwardRef(({ className, ...rest }, ref: Ref<InputRef>) => (
+export interface InlineInputProps
+  extends InputHTMLAttributes<HTMLInputElement> {
+  /**
+   * Additional className for the input container
+   */
+  className?: string;
+
+  /**
+   * Additional width for input element
+   */
+  extraWidth?: number | string;
+
+  /**
+   * Additional className for the input element
+   */
+  inputClassName?: string;
+
+  /**
+   * Don't collapse size to less than the placeholder
+   */
+  placeholderIsMinWidth?: boolean;
+
+  /**
+   * onAutosize handler
+   */
+  onAutosize?: (newWidth: number) => void;
+}
+
+export const InlineInput: FC<InlineInputProps> = forwardRef(
+  (
+    { inputClassName, placeholderIsMinWidth = true, ...rest },
+    ref: Ref<InputRef>
+  ) => (
     <AutosizeInput
-      ref={ref}
-      className={classNames(css.input, className)}
-      placeholderIsMinWidth
+      inputRef={ref}
+      inputClassName={classNames(css.input, inputClassName)}
+      placeholderIsMinWidth={placeholderIsMinWidth}
       {...rest}
     />
-  ));
+  )
+);
