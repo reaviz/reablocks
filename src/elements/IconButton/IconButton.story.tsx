86a5a652fa47677c04fcf3e4053a03ef556e141f
Shaswat <shaswat.dba@gmail.com>
2023-11-01 14:35:15 -0700

    refactored IconButton

--- src/elements/IconButton/IconButton.story.tsx
+++ src/elements/IconButton/IconButton.story.tsx
@@ -11,73 +11,117 @@ const BellIcon = () => (
     xmlns="http://www.w3.org/2000/svg"
     width="16"
     height="16"
-    fill="white"
+    fill="currentColor"
     viewBox="0 0 16 16"
   >
     <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z" />
   </svg>
 );
 
-export const IconAndTextSizes = () => (
+export const Sizes = () => (
   <Fragment>
-    <IconButton size="small" icon={<BellIcon />}>
-      Small
+    <IconButton size="small">
+      <BellIcon />
     </IconButton>
     {` `}
-    <IconButton size="medium" icon={<BellIcon />}>
-      Medium
+    <IconButton size="medium">
+      <BellIcon />
     </IconButton>
     {` `}
-    <IconButton size="large" icon={<BellIcon />}>
-      Large
+    <IconButton size="large">
+      <BellIcon />
     </IconButton>
   </Fragment>
 );
 
-export const IconAndTextPositions = () => (
-  <Fragment>
-    <IconButton size="medium" icon={<BellIcon />} direction="row">
-      Text After Icon
-    </IconButton>
-    {` `}
-    <IconButton size="medium" icon={<BellIcon />} direction="column">
-      Text Below Icon
-    </IconButton>
-    {` `}
-    <IconButton size="medium" icon={<BellIcon />} direction="rowReverse">
-      Text Before Icon
-    </IconButton>
-    {` `}
-    <IconButton size="medium" icon={<BellIcon />} direction="columnReverse">
-      Text Above Icon
-    </IconButton>
-  </Fragment>
-);
-
-export const IconOnly = () => (
+export const ColorsBackground = () => (
   <div style={{ display: 'flex', gap: 10, alignItems: 'center' }}>
-    <IconButton size="small" icon={<BellIcon />} />
-    <IconButton size="medium" icon={<BellIcon />} />
-    <IconButton size="large" icon={<BellIcon />} />
+    <IconButton size="medium" variant="filled">
+      <BellIcon />
+    </IconButton>
+    <IconButton size="medium" variant="filled" color="primary">
+      <BellIcon />
+    </IconButton>
+    <IconButton size="medium" variant="filled" color="secondary">
+      <BellIcon />
+    </IconButton>
+    <IconButton size="medium" variant="filled" color="success">
+      <BellIcon />
+    </IconButton>
+    <IconButton size="medium" variant="filled" color="warning">
+      <BellIcon />
+    </IconButton>
+    <IconButton size="medium" variant="filled" color="error">
+      <BellIcon />
+    </IconButton>
+    <IconButton size="medium" variant="filled" disabled>
+      <BellIcon />
+    </IconButton>
   </div>
 );
 
-export const IconButtonColors = () => (
+export const ColorsIcons = () => (
   <div style={{ display: 'flex', gap: 10, alignItems: 'center' }}>
-    <IconButton size="medium" icon={<BellIcon />} />
-    <IconButton size="medium" color="primary" icon={<BellIcon />} />
-    <IconButton size="medium" color="secondary" icon={<BellIcon />} />
-    <IconButton size="medium" color="success" icon={<BellIcon />} />
-    <IconButton size="medium" color="warning" icon={<BellIcon />} />
-    <IconButton size="medium" color="error" icon={<BellIcon />} />
-    <IconButton size="medium" disabled icon={<BellIcon />} />
+    <IconButton size="medium" variant="text">
+      <BellIcon />
+    </IconButton>
+    <IconButton size="medium" variant="text" color="primary">
+      <BellIcon />
+    </IconButton>
+    <IconButton size="medium" variant="text" color="secondary">
+      <BellIcon />
+    </IconButton>
+    <IconButton size="medium" variant="text" color="success">
+      <BellIcon />
+    </IconButton>
+    <IconButton size="medium" variant="text" color="warning">
+      <BellIcon />
+    </IconButton>
+    <IconButton size="medium" variant="text" color="error">
+      <BellIcon />
+    </IconButton>
+    <IconButton size="medium" variant="text" disabled>
+      <BellIcon />
+    </IconButton>
   </div>
 );
 
-export const IconButtonVariants = () => (
+export const ColorsOutline = () => (
   <div style={{ display: 'flex', gap: 10, alignItems: 'center' }}>
-    <IconButton size="medium" icon={<BellIcon />} />
-    <IconButton size="medium" variant="outline" icon={<BellIcon />} />
-    <IconButton size="medium" variant="text" icon={<BellIcon />} />
+    <IconButton size="medium" variant="outline">
+      <BellIcon />
+    </IconButton>
+    <IconButton size="medium" variant="outline" color="primary">
+      <BellIcon />
+    </IconButton>
+    <IconButton size="medium" variant="outline" color="secondary">
+      <BellIcon />
+    </IconButton>
+    <IconButton size="medium" variant="outline" color="success">
+      <BellIcon />
+    </IconButton>
+    <IconButton size="medium" variant="outline" color="warning">
+      <BellIcon />
+    </IconButton>
+    <IconButton size="medium" variant="outline" color="error">
+      <BellIcon />
+    </IconButton>
+    <IconButton size="medium" variant="outline" disabled>
+      <BellIcon />
+    </IconButton>
+  </div>
+);
+
+export const Variants = () => (
+  <div style={{ display: 'flex', gap: 10, alignItems: 'center' }}>
+    <IconButton size="medium">
+      <BellIcon />
+    </IconButton>
+    <IconButton size="medium" variant="outline">
+      <BellIcon />
+    </IconButton>
+    <IconButton size="medium" variant="text">
+      <BellIcon />
+    </IconButton>
   </div>
 );
--- src/elements/IconButton/IconButton.tsx
+++ src/elements/IconButton/IconButton.tsx
@@ -1,58 +1,13 @@
 import React, { FC, forwardRef, Ref } from 'react';
-import classNames from 'classnames';
-import css from './IconButton.module.css';
 import { Button, ButtonProps, ButtonRef } from '../Button';
-import { Stack } from '../../layout';
 
-export interface IconButtonProps extends ButtonProps {
-  /**
-   * The icon inside the Button.
-   */
-  icon: React.ReactElement;
-
-  /**
-   * The direction of the icon and text layout.
-   */
-  direction?: 'row' | 'column' | 'rowReverse' | 'columnReverse';
-
-  /**
-   * CSS Classname to apply to the icon
-   */
-  iconClassName?: string;
-}
+export interface IconButtonProps
+  extends Omit<ButtonProps, 'fullWidth' | 'startIcon' | 'endIcon'> {}
 
 export const IconButton: FC<IconButtonProps & ButtonRef> = forwardRef(
-  (
-    {
-      className,
-      children,
-      icon,
-      direction = 'row',
-      iconClassName,
-      size,
-      variant = 'filled',
-      color = 'default',
-      ...rest
-    }: IconButtonProps,
-    ref: Ref<HTMLButtonElement>
-  ) => (
-    <Button
-      {...rest}
-      ref={ref}
-      className={classNames(className, css.iconBtn, {
-        [css.hasWords]: children !== undefined,
-        [css[color]]: true,
-        [css[size]]: true,
-        [css[variant]]: true
-      })}
-      size={size}
-      color={color}
-      variant={variant}
-    >
-      <Stack direction={direction} dense className={css.childrenContainer}>
-        <div className={iconClassName}>{icon}</div>
-        {children && <div>{children}</div>}
-      </Stack>
+  ({ children, ...rest }: IconButtonProps, ref: Ref<HTMLButtonElement>) => (
+    <Button {...rest} ref={ref}>
+      {children}
     </Button>
   )
 );
