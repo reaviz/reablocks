6d8d66ecf8426d2ca50cb10ff0852cac4b09f074
SerhiiTsybulskyi <c4r0n0s@gmail.com>
2024-03-05 10:12:57 +0200

    update select group style

--- src/form/Select/SelectMenu/SelectMenuTheme.ts
+++ src/form/Select/SelectMenu/SelectMenuTheme.ts
@@ -14,7 +14,7 @@ export interface SelectMenuTheme {
 const baseTheme: SelectMenuTheme = {
   base: 'border border-solid rounded-b-md text-center will-change-[transform,opacity] min-w-[112px] max-h-[300px] overflow-y-auto text-left box-border',
   groupItem: 'p-0 border-0',
-  groupTitle: 'text-xs font-bold uppercase m-0;',
+  groupTitle: 'text-xs font-bold uppercase m-0 px-1.5 py-2.5',
   option: {
     base: 'text-sm flex-1 whitespace-break-spaces break-words py-1.5 px-2.5',
     hover: '',
@@ -27,6 +27,7 @@ const baseTheme: SelectMenuTheme = {
 export const lightSelectMenuTheme: SelectMenuTheme = {
   ...baseTheme,
   base: [baseTheme.base, 'bg-white border-gray-400 text-black'].join(' '),
+  groupTitle: [baseTheme.groupTitle, 'text-gray-500'].join(' '),
   option: {
     ...baseTheme.option,
     base: [baseTheme.option.base, 'text-black'].join(' '),
@@ -42,6 +43,7 @@ export const lightSelectMenuTheme: SelectMenuTheme = {
 export const darkSelectMenuTheme: SelectMenuTheme = {
   ...baseTheme,
   base: [baseTheme.base, 'bg-neutral-900', 'border-gray-700'].join(' '),
+  groupTitle: [baseTheme.groupTitle, 'text-gray-600'].join(' '),
   option: {
     ...baseTheme.option,
     base: [baseTheme.option.base, 'text-white'].join(' '),
