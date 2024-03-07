65593655fd995cdb800848f15b5d3c35923e362a
SerhiiTsybulskyi <c4r0n0s@gmail.com>
2024-03-07 10:22:09 +0200

    update CalendarTheme

--- src/form/Calendar/CalendarRangeTheme.ts
+++ src/form/Calendar/CalendarRangeTheme.ts
@@ -1,42 +1,31 @@
-import { CalendarTheme } from './CalendarTheme';
+import {
+  CalendarTheme,
+  darkCalendarTheme,
+  lightCalendarTheme
+} from './CalendarTheme';
 
 export interface CalendarRangeTheme
   extends Omit<CalendarTheme, 'months' | 'years'> {}
 
-const baseTheme: CalendarRangeTheme = {
+const baseTheme: Partial<CalendarRangeTheme> = {
   base: 'relative overflow-hidden',
   header: 'flex text-center justify-between mb-2 items-center',
   title: 'font-semibold flex grow justify-around',
-  content: 'flex gap-4',
-
-  days: {
-    header: 'text-center grid grid-cols-7 opacity-50 mb-1 border-t pt-2',
-    dayOfWeek: 'text-center font-bold',
-    week: 'grid grid-cols-7',
-    day: 'flex p-2',
-    outside: '',
-    startRangeDate: 'rounded-tl-md rounded-tr-none rounded-br-none',
-    cornerStartDateBottom: 'rounded-bl-none',
-    endRangeDate: 'rounded-br-md rounded-bl-none rounded-tl-none',
-    cornerEndDateTop: 'rounded-tr-none',
-    range: 'rounded-none'
-  }
+  content: 'flex gap-4'
 };
 
 export const lightCalendarRangeTheme: CalendarRangeTheme = {
-  ...baseTheme,
-  days: {
-    ...baseTheme.days,
-    header: [baseTheme.days.header, 'border-slate-700 text-black'].join(' '),
-    outside: [baseTheme.days.outside, 'text-black/60'].join(' ')
-  }
+  base: baseTheme.base,
+  header: baseTheme.header,
+  title: baseTheme.title,
+  content: baseTheme.content,
+  days: lightCalendarTheme.days
 };
 
 export const darkCalendarRangeTheme: CalendarRangeTheme = {
-  ...baseTheme,
-  days: {
-    ...baseTheme.days,
-    header: [baseTheme.days.header, 'border-slate-500 text-white'].join(' '),
-    outside: [baseTheme.days.outside, 'text-gray-100/60'].join(' ')
-  }
+  base: baseTheme.base,
+  header: baseTheme.header,
+  title: baseTheme.title,
+  content: baseTheme.content,
+  days: darkCalendarTheme.days
 };
