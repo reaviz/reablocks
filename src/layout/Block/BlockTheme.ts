31611dc2c4cda0db690989b33358229a87b3e0b5
SerhiiTsybulskyi <c4r0n0s@gmail.com>
2024-03-11 14:28:55 +0200

    migrate Block component

--- src/layout/Block/BlockTheme.ts
+++ src/layout/Block/BlockTheme.ts
@@ -1,11 +1,33 @@
 export interface BlockTheme {
   base: string;
   disableMargin: string;
+  label: string;
+  centerAlign: string;
+  endAlign: string;
+  horizontal: {
+    base: string;
+    label: string;
+  };
+  vertical: {
+    base: string;
+    label: string;
+  };
 }
 
 const baseTheme: BlockTheme = {
   base: 'mb-2.5',
-  disableMargin: 'mb-0'
+  disableMargin: 'mb-0',
+  label: 'text-xs',
+  centerAlign: 'items-center',
+  endAlign: 'items-end',
+  horizontal: {
+    base: 'flex flex-row items-baseline',
+    label: 'mr-0.5 whitespace-nowrap'
+  },
+  vertical: {
+    base: 'block',
+    label: 'block mb-0.5'
+  }
 };
 
 export const lightBlockTheme: BlockTheme = {
