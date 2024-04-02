009b2afac86339a6965535ae8dddddc1fb5ddf55
amcdnl <amcdaniel2@gmail.com>
2024-04-02 17:54:56 -0400

    add rest of tw sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -49,9 +49,19 @@ const config: Config = {
       mono: ['Fira Code', 'monospace']
     },
     fontSize: {
-      sm: '12px',
-      base: '14px',
-      lg: '16px'
+      sm: '0.75rem', // 12px
+      base: '0.875rem', // 14px
+      lg: '1rem', // 16px
+      // Rest are defaults
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+      '6xl': '3.75rem',
+      '7xl': '4.5rem',
+      '8xl': '6rem',
+      '9xl': '8rem',
     },
     extend: {
       colors: {
