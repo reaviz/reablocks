2221b90a6cd8b389af1d0c0defa9f4c4b71e98b9
amcdnl <amcdaniel2@gmail.com>
2024-04-04 11:47:27 -0400

    profile start

--- docs/blocks/administration/Profile.tsx
+++ docs/blocks/administration/Profile.tsx
@@ -5,108 +5,61 @@ import { Input } from '../../../src/form/Input';
 import { Button } from '../../../src/elements/Button';
 import { Block } from '../../../src/layout/Block';
 import { Avatar } from '../../../src/elements/Avatar';
+import { motion } from 'framer-motion';
+import { Card, Divider } from '../../../src/layout';
+import logo from '../../assets/reaviz.svg';
 
 export const Profile = () => {
-  const { control, handleSubmit, formState: { isSubmitting } } = useForm();
+  // const { control, handleSubmit, formState: { isSubmitting } } = useForm();
 
   return (
-    <Stack className="w-full gap-2 p-4" alignItems="start">
-      <aside className="md:w-1/3 lg:w-1/4 ">
-        <List>
-          <ListItem active onClick={() => null}>Profile</ListItem>
-          <ListItem onClick={() => null}>Groups</ListItem>
-          <ListItem onClick={() => null}>Roles</ListItem>
-          <ListItem onClick={() => null}>About</ListItem>
-        </List>
-      </aside>
-      <div className="w-full md:w-2/3 lg:w-3/4">
-        <form onSubmit={handleSubmit(values => console.log('values', values))}>
-          <Avatar name="Austin McDaniel" size={64} />
-          <br />
-          <Stack>
-            <Block className="w-full">
-              <Controller
-                name="firstName"
-                control={control}
-                render={({ field: { value, onBlur, onChange } }) => (
-                  <Input
-                    name="firstName"
-                    disabled={isSubmitting}
-                    placeholder="First Name"
-                    value={value}
-                    fullWidth
-                    type="text"
-                    onChange={onChange}
-                    onBlur={onBlur}
-                  />
-                )}
-              />
-            </Block>
-            <Block className="w-full">
-              <Controller
-                name="lastName"
-                control={control}
-                render={({ field: { value, onBlur, onChange } }) => (
-                  <Input
-                    name="lastName"
-                    disabled={isSubmitting}
-                    placeholder="Last Name"
-                    value={value}
-                    fullWidth
-                    type="text"
-                    onChange={onChange}
-                    onBlur={onBlur}
-                  />
-                )}
-              />
-            </Block>
+    <motion.div
+      initial={{ y: -20, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      className="mt-10 mb-10 sm:mx-auto sm:w-full sm:max-w-sm md:max-w-[600px]"
+    >
+      <Card className="w-full p-12">
+        <Stack>
+          <img src={logo} alt="Logo" className="h-11 w-auto mb-2" />
+          <div>
+            <h2 className="text-xl font-sans font-bold">Good Code</h2>
+            <p className="opacity-60">reablocks.io/goodcode</p>
+          </div>
+        </Stack>
+        <h1 className="mt-10 text-2xl font-sans font-bold">
+          Profile Settings
+        </h1>
+        <p className="opacity-60">Customize and edit essential profile details.</p>
+        <Divider />
+        <Stack>
+          <Stack direction="column" alignItems="start" dense>
+            <h3 className="text-1xl font-sans font-bold">Public Profile</h3>
+            <p className="opacity-60 text-sm">This will be displayed on your profile.</p>
           </Stack>
-          <Block>
-            <Controller
-              name="email"
-              control={control}
-              render={({ field: { value, onBlur, onChange } }) => (
-                <Input
-                  name="email"
-                  disabled={isSubmitting}
-                  placeholder="Email"
-                  value={value}
-                  type="email"
-                  onChange={onChange}
-                  onBlur={onBlur}
-                />
-              )}
-            />
-          </Block>
-          <Block>
-            <Controller
-              name="company"
-              control={control}
-              render={({ field: { value, onBlur, onChange } }) => (
-                <Input
-                  name="company"
-                  disabled={isSubmitting}
-                  placeholder="Company"
-                  value={value}
-                  type="text"
-                  onChange={onChange}
-                  onBlur={onBlur}
-                />
-              )}
-            />
-          </Block>
-          <Button
-            type="submit"
-            fullWidth
-            variant="filled"
-            color="primary"
-            className="mb-2"
-            disabled={isSubmitting}
-          >
-            {isSubmitting ? 'Saving...' : 'Save'}
-          </Button>
-        </form>
-      </div>
-    </Stack>
+          <Stack>
+            <Input value="Good Code" />
+          </Stack>
+        </Stack>
+        <Divider />
+        <Stack>
+          <Stack direction="column" alignItems="start" dense>
+            <h3 className="text-1xl font-sans font-bold">Company Logo</h3>
+            <p className="opacity-60 text-sm">Min 400 x 400 px, PNG or JPEG formats.</p>
+          </Stack>
+          <Avatar name="Austin M" size="xl" />
+        </Stack>
+        <Divider />
+        <Stack>
+          <Stack direction="column" alignItems="start" dense>
+            <h3 className="text-1xl font-sans font-bold">Branding</h3>
+            <p className="opacity-60 text-sm">Add your logo to reports and emails.</p>
+          </Stack>
+        </Stack>
+        <Divider />
+        <Stack>
+          <h3 className="text-1xl font-sans font-bold">Social Profiles</h3>
+        </Stack>
+      </Card>
+    </motion.div>
   );
 }
