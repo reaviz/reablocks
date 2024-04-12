6c43dbee5fe5ffe6f140b7cf7d92e64c00de68de
amcdnl <amcdaniel2@gmail.com>
2024-04-12 09:54:33 -0400

    fix backdrop

--- src/layers/Backdrop/Backdrop.story.tsx
+++ src/layers/Backdrop/Backdrop.story.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Backdrop } from './Backdrop';
 
 const meta: Meta<typeof Backdrop> = {
-  title: 'Examples/Backdrop',
+  title: 'Components/Layers/Backdrop',
   component: Backdrop
 };
 
