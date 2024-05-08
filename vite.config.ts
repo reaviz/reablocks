d5aee8d8826f9750b8c6b4fa1aae127beb0d9dc9
amcdnl <amcdaniel2@gmail.com>
2024-05-08 07:51:17 -0700

    remove copy

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ import { resolve } from 'path';
 import external from 'rollup-plugin-peer-deps-external';
 import dts from 'vite-plugin-dts';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig(({ mode }) =>
   mode === 'library'
@@ -25,14 +24,6 @@ export default defineConfig(({ mode }) =>
         }),
         checker({
           typescript: true
-        }),
-        viteStaticCopy({
-          targets: [
-            {
-              src: 'src/**/*.story.tsx',
-              dest: 'stories/'
-            }
-          ]
         })
       ],
       test: {
