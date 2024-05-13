d0f5cad19e0e4cc039c4b7f1eb9b16827e9efb2d
Grant Steffen <gsteffen1203@gmail.com>
2024-05-13 14:19:28 -0400

    Update BreadcrumbSeparator default children

--- src/layout/Breadcrumbs/BreadcrumbSeparator.tsx
+++ src/layout/Breadcrumbs/BreadcrumbSeparator.tsx
@@ -1,18 +1,7 @@
+import { Arrow } from '@/elements';
 import { cn, useComponentTheme } from '@/utils';
 import React, { FC } from 'react';
 import { BreadcrumbsTheme } from './BreadcrumbsTheme';
-
-const Chevron = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="16"
-    height="16"
-    viewBox="0 0 16 16"
-    fill="currentColor"
-  >
-    <path d="M6.47003 4L5.53003 4.94L8.58336 8L5.53003 11.06L6.47003 12L10.47 8L6.47003 4Z" />
-  </svg>
-);
 
 export interface BreadcrumbSeparatorProps
   extends React.LiHTMLAttributes<HTMLLIElement> {
@@ -20,7 +9,7 @@ export interface BreadcrumbSeparatorProps
 }
 
 export const BreadcrumbSeparator: FC<BreadcrumbSeparatorProps> = ({
-  children,
+  children = <Arrow direction="right" />,
   className,
   theme: customTheme,
   ...rest
@@ -34,7 +23,7 @@ export const BreadcrumbSeparator: FC<BreadcrumbSeparatorProps> = ({
       className={cn(theme.separator, className)}
       {...rest}
     >
-      {children ?? <Chevron />}
+      {children}
     </li>
   );
 };
