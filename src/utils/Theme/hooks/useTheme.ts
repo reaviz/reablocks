346e1feb9437f70fea4b794a336c458fd769f957
amcdnl <amcdaniel2@gmail.com>
2024-05-20 12:18:43 -0400

    improve various comments

--- src/utils/Theme/hooks/useTheme.ts
+++ src/utils/Theme/hooks/useTheme.ts
@@ -1,6 +1,10 @@
 import { useContext } from 'react';
 import { ThemeContext, ThemeContextProps } from '@/utils/Theme/ThemeProvider';
 
+/**
+ * Hook to get the theme context.
+ * @returns ThemeContextProps
+ */
 export const useTheme = (): ThemeContextProps | null => {
   const context = useContext(ThemeContext);
 
