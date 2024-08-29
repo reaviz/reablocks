3b814614a658a24eb1f64d8f9897c19c741d56ae
Stephanie Yang <steph@goodcode.us>
2024-08-29 18:33:08 -0500

    add abilty to control icon button sizes independent of button size

--- src/elements/IconButton/IconButton.tsx
+++ src/elements/IconButton/IconButton.tsx
@@ -1,13 +1,33 @@
 import React, { FC, forwardRef, Ref } from 'react';
 import { Button, ButtonProps, ButtonRef } from '@/elements/Button';
+import { twMerge } from 'tailwind-merge';
+import { useComponentTheme } from '@/utils';
 
 export interface IconButtonProps
   extends Omit<ButtonProps, 'fullWidth' | 'startAdornment' | 'endAdornment'> {}
 
 export const IconButton: FC<IconButtonProps & ButtonRef> = forwardRef(
-  ({ children, ...rest }: IconButtonProps, ref: Ref<HTMLButtonElement>) => (
-    <Button {...rest} ref={ref}>
-      {children}
-    </Button>
-  )
+  (
+    {
+      children,
+      className,
+      size = 'medium',
+      theme: customTheme,
+      ...rest
+    }: IconButtonProps,
+    ref: Ref<HTMLButtonElement>
+  ) => {
+    const theme = useComponentTheme('button', customTheme);
+
+    return (
+      <Button
+        className={twMerge(theme.iconSizes[size], className)}
+        size={size}
+        {...rest}
+        ref={ref}
+      >
+        {children}
+      </Button>
+    );
+  }
 );
