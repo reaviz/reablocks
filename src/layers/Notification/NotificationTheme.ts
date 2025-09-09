63d7d0ea3882b1fd5eb2e8bf350bb234c3816bce
Evgeniy <evgeniy@goodcode.us>
2025-09-09 11:09:05 +0300

    Use dimensions for Notification component

--- src/layers/Notification/NotificationTheme.ts
+++ src/layers/Notification/NotificationTheme.ts
@@ -42,7 +42,8 @@ export const notificationTheme: NotificationTheme = {
     'fixed z-9998 h-auto -translate-x-2/4 mb-1 px-24 py-0 left-2/4 bottom-0',
   notification: {
     base: `
-          flex relative text-sm min-w-[400px] rounded-md mb-2.5 py-2 px-4 border backdrop-blur-xl transition-colors
+          flex relative text-sm min-w-[400px] rounded-(--notifications-details-corner-radius-primary)
+          mb-2.5 p-(--notifications-details-vertical-padding-standard-sm) border backdrop-blur-xl transition-colors
           border-notifications-colors-stroke-neutral-resting hover:border-notifications-colors-stroke-neutral-hover
           bg-notifications-colors-background-neutral-resting hover:bg-notifications-colors-background-neutral-hover
         `,
@@ -55,37 +56,40 @@ export const notificationTheme: NotificationTheme = {
           border-notifications-colors-stroke-success-resting hover:border-notifications-colors-stroke-success-hover
           bg-notifications-colors-background-success-resting hover:bg-notifications-colors-background-success-hover
         `,
-        icon: 'text-notifications-colors-assets-success-resting size-4'
+        icon: 'text-notifications-colors-assets-success-resting size-(--notifications-details-asset-size-base)'
       },
       error: {
         base: `
           border-notifications-colors-stroke-error-resting hover:border-notifications-colors-stroke-error-hover
           bg-notifications-colors-background-error-resting hover:bg-notifications-colors-background-error-hover
         `,
-        icon: 'text-notifications-colors-assets-destructive-resting size-4'
+        icon: 'text-notifications-colors-assets-destructive-resting size-(--notifications-details-asset-size-base)'
       },
       warning: {
         base: `
           border-notifications-colors-stroke-warning-resting hover:border-notifications-colors-stroke-warning-hover
           bg-notifications-colors-background-warning-resting hover:bg-notifications-colors-background-warning-hover
         `,
-        icon: 'text-notifications-colors-assets-warning-static size-4'
+        icon: 'text-notifications-colors-assets-warning-static size-(--notifications-details-asset-size-base)'
       },
       info: {
         base: `
           border-notifications-colors-stroke-info-resting hover:border-notifications-colors-stroke-info-hover
           bg-notifications-colors-background-info-resting hover:bg-notifications-colors-background-info-hover
         `,
-        icon: 'text-notifications-colors-assets-info-resting size-4'
+        icon: 'text-notifications-colors-assets-info-resting size-(--notifications-details-asset-size-base)'
       }
     },
     content: 'inline-flex items-start flex-1 flex-col justify-center',
     header:
-      'text-sm flex gap-2 items-center text-notifications-colors-text-title-resting',
+      'text-sm flex gap-(--notifications-details-space-between-standard-sm) items-center text-notifications-colors-text-title-resting',
     closeContainer: 'inline-flex items-center',
-    action: 'ml-auto mr-2 items-center flex',
-    closeButton:
-      'cursor-pointer text-xs font-semibold m-0 border-0 text-text-primary hover:text-text-primary/70',
-    body: 'text-xxs mt-1 text-notifications-colors-text-normal-resting'
+    action:
+      'ml-auto mr-(--notifications-details-space-between-standard-sm) items-center flex',
+    closeButton: `
+      size-(--notifications-details-asset-size-compact) cursor-pointer text-xs font-semibold
+      m-0 border-0 text-notifications-colors-assets-normal-resting hover:opacity-70 transition-opacity
+    `,
+    body: 'text-xxs mt-(--notifications-details-space-between-standard-sm) text-notifications-colors-text-normal-resting'
   }
 };
