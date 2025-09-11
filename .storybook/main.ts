08d3316243dfd588a254a8d99d2ac0df033debe5
Evgeniy <evgeniy@goodcode.us>
2025-09-11 12:25:07 +0300

    trigger build

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,20 +6,17 @@ const config: StorybookConfig = {
     '../docs/**/*.story.tsx',
     '../src/**/*.story.tsx'
   ],
-
   addons: [
     '@storybook/addon-storysource',
     '@storybook/addon-essentials',
     '@storybook/addon-themes',
     '@storybook/addon-interactions',
     '@storybook/addon-docs',
-],
-
+  ],
   framework: {
     name: '@storybook/react-vite',
     options: {}
   },
-
   typescript: {
     reactDocgen: 'react-docgen-typescript'
   }
