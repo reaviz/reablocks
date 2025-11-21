3ec2e8478cfa669909383c6ff92383be71f76a6b
Evgeniy <evgeniy@goodcode.us>
2025-11-21 15:13:41 +0200

    Fix Skeleton theme

--- src/elements/Skeleton/SkeletonTheme.ts
+++ src/elements/Skeleton/SkeletonTheme.ts
@@ -13,7 +13,7 @@ export interface SkeletonTheme {
 export const skeletonTheme: SkeletonTheme = {
   base: 'rounded-md bg-background-neutral-raised-4',
   animated:
-    'animate-[pulse_1.5s_ease-in-out_infinite] light:bg-gradient-to-r from-background-neutral-raised-3 via-background-neutral-raised-4 to-background-neutral-raised-3 bg-[length:200%_100%]',
+    'animate-[pulse_1.5s_ease-in-out_infinite] bg-gradient-to-r from-background-neutral-raised-3 via-background-neutral-raised-4 to-background-neutral-raised-3 bg-[length:200%_100%]',
   variants: {
     text: 'h-4 w-full',
     rounded: 'rounded-full w-10 h-10',
