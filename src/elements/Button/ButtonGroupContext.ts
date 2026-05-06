9f0ab2886174045f8087c1670035ee899a0565d3
SerhiiTsybulskiy <c4r0n0s@gmail.com>
2026-05-06 13:20:07 +0300

    Update ButtonGroupContextProps to allow string variants and sizes

--- src/elements/Button/ButtonGroupContext.ts
+++ src/elements/Button/ButtonGroupContext.ts
@@ -4,12 +4,12 @@ export interface ButtonGroupContextProps {
   /**
    * Style variant of the buttons.
    */
-  variant?: 'filled' | 'outline' | 'text';
+  variant?: 'filled' | 'outline' | 'text' | string;
 
   /**
    * The size variation of the buttons.
    */
-  size?: 'small' | 'medium' | 'large';
+  size?: 'small' | 'medium' | 'large' | string;
 }
 
 export const ButtonGroupContext = createContext<ButtonGroupContextProps>({
